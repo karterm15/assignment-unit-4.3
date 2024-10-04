25940a16d1444e72c9e7d8a42cb632259a1269dd
Karter Moua <kartermoua151@gmail.com>
2024-10-04 16:40:01 -0600

    unit 4 part 3 cart system

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -2,11 +2,37 @@ console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
 
+let basket = [];
+const maxItems = 5;
 
+function addItem  ( item ){
+    if (isFull()){
+        console.log( 'there is no room');
+        return false;
+    }
+    console.log( 'addItem', item);
+     basket.push( item );
+     return true;
+}
 
+function listItems(){
+    for (let i=0; i < basket.length; i++){
+        console.log(basket[i]);
+    }
+    return true;
+}
 
+function empty(){
+    console.log( 'empty');
+    basket.length = 0;
+}
 
-
+function isFull(){
+    if (basket.length < maxItems){
+        return false;
+    } else (basket.length >= maxItems)
+        return true;
+}
 
 
 
